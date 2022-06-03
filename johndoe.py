b70b38b0b34aa708974d9cc2b8c6aa8c3597f4b7
ian umney <myemail@ianumney.com>
2022-06-03 12:05:19 +0100

    Fix lambda expression

--- johndoe.py
+++ johndoe.py
@@ -102,7 +102,7 @@ class JohnDoe:
         """Gets string with the format of a national insurance
         number: AB123456C"""
         # random ascii char
-        rac = lambda: random.choice(string.ascii_uppercase)
+        rac: () = lambda: random.choice(string.ascii_uppercase)
 
         return f"{rac()}{rac()}{random.randint(111_111, 999_999)}{rac()}"
 
