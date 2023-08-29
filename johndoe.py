1ba696d3160426ed928c0ff60fc9a87174bb1d92
Ian Umney <ianumney@icloud.com>
2023-08-30 00:54:45 +0100

    Updates

--- johndoe.py
+++ johndoe.py
@@ -1,13 +1,13 @@
 import random
 import string
-import datetime
-import identity_documents
+from datetime import datetime
 import argparse
 import json
 from src import nino
 from src import national_identity_card as nic
 import driving_license
 import bank
+import re
 
 
 class JohnDoe:
@@ -18,32 +18,45 @@ class JohnDoe:
         Parameters:
         - gender (str): Gender of the individual ('m' or 'f').
         - name (str): Full name of the individual.
-        - age (str): Age of the individual (default is random between 18 and 65).
-        - documents (bool): Whether to generate identity documents (default is False).
+        - dob (str): Date of birth in DD/MM/YYYY format
+        - docs (bool): Whether to generate identity documents (default is False).
          """
 
         self.gender = self.gender(kwargs.get("gender"))  # Must be first as self.name relies on gender (m\f)
         self.name = self.name(kwargs.get("name"))
-        self.age = self.age(kwargs.get("age"))
 
-        if kwargs.get("document"):
-            self.document()
-            print("TRUE")
+        if not kwargs.get("dob"):
+            self.date_of_birth = self.date_of_birth(None)
+        else:
+            self.date_of_birth = self.date_of_birth(kwargs.get("dob"))
+
         self.car = self.car()
         self.nino = self.nino()
         self.email = self.email()
-        # self.image = self.image()
         self.address = self.address()
-        self.birthday = self.birthday()
         self.banking = self.banking_info()
         self.ip_address = self.ip_address()
         self.mobile_number = self.mobile_number()
         self.driving_license = self.driving_license()
+        if kwargs.get("docs"):
+            self.document()
+        if kwargs.get("print"):
+            self.print()
 
     @staticmethod
-    def car():
+    def car() -> dict:
+        """Generates car information for JD.
+
+        Returns:
+            Dictionary: associated information for vehicle.
+        """
 
         def number_plate():
+            """Generate random number plate of UK format AB01 23C.
+
+            Returns:
+                String: Random letters and numbers.
+            """
             letters = string.ascii_letters
             digits = string.digits
 
@@ -59,7 +72,17 @@ class JohnDoe:
 
     @staticmethod
     def gender(gender):
-        """Check is the gender has been given, is correct, or assign a random gender."""
+        """Assigns JD's gender by either parsing input or randomly generating.
+
+        Parameters:
+            gender: Must be either 'male' or 'female', or 'None' to generate random gender.
+
+        Returns:
+            String: 0th index of 'male' or 'female'.
+
+        Note:
+            gender should be expressly be given when a name has been given otherwise the gender may not match the name.
+        """
 
         if gender is not None and gender[0].lower() in ["m", "f"]:
             return gender[0].lower()
@@ -75,8 +98,15 @@ class JohnDoe:
             else:
                 return "f"
 
-    def name(self, name):
-        """Handle naming the person from input or generation"""
+    def name(self, name) -> str:
+        """Assigns JD's name by parsing input or randomly generating one.
+
+        Parameters:
+            name: Can be first name only, or first and last name.
+
+        Returns:
+            String: First and last name.
+        """
 
         def random_firstname():
             with open(f"./src/names/{self.gender}.txt") as forename_file:
@@ -110,26 +140,67 @@ class JohnDoe:
             return generated_name
 
     @staticmethod
-    def age(age) -> int:
+    def date_of_birth(dob) -> str:
+        """Assigns JD's date of birth by parsing input or randomly generating.
 
-        if age is not None and age < 18:
-            raise ValueError(f"You cannot generate minors! Nonce.")
+        Parameters:
+            dob: Date of birth in the format DD/MM/YYYY.
 
-        elif age is not None and age >= 18:
+        Returns:
+            String: Date of birth in the format DD/MM/YYYY.
+        """
+
+        def valid_dob(input_dob):
+            """Checks id the input dob is in the valid format"""
+
+            pattern = re.compile(r'^\d{2}/\d{2}/\d{4}$')
+
+            if pattern.match(input_dob):
+                day, month, year = map(int, input_dob.split('/'))
+                if 1 <= day <= 31 and 1 <= month <= 12:
+                    return True
+            return False
+
+        def calculate_age(birthdate):
+            """Calculates age from birthdate to determine under-age generation"""
+
+            today = datetime.today()
+            birthdate = datetime.strptime(birthdate, '%d/%m/%Y')
+            age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
             return age
 
-        else:
-            return random.randint(18, 80)
+        def generate_dob():
+            """Generate a random date of birth"""
 
-    def json(self):
+            random_day = random.randint(1, 27)
+            random_month = random.randint(1, 11)
+            random_year = random.randint(datetime.now().year - 88, datetime.now().year - 18)
+            date_of_birth = f"{random_month}/{random_day}/{random_year}"
+            return date_of_birth
+
+        if dob is None:
+            return generate_dob()
+
+        elif valid_dob(dob):
+            if calculate_age(dob) <= 18:
+                raise ValueError(f"You cannot generate minors! Odep.")
+            else:
+                return dob
+        else:
+            exit("The date of birth should be in the format of 'DD/MM/YYY'")
+
+    def json(self) -> json:
         """Print the JohnDoe object information"""
 
         return json.dumps(self.__dict__, indent=4)
 
     @staticmethod
-    def mobile_number():
-        """Get a random phone number in UK format using
-        genuine prefixes and providers"""
+    def mobile_number() -> dict:
+        """Assigns JD's mobile phone information based on accurate data.
+
+        Returns:
+            Dictionary: Randomly generated phone number and random provider from list.
+        """
 
         with open(f"./src/mobile_numbers.txt", "r") as file:
             # Get random line from number file
@@ -148,9 +219,13 @@ class JohnDoe:
         return {"number": number, "provider": provider}
 
     @staticmethod
-    def nino():
+    def nino() -> str:
         # todo Add temp prefixes from https://www.gov.uk/hmrc-internal-manuals/national-insurance-manual/nim39110
-        """Create a national insurance number with format QQ 12 34 56 C"""
+        """Assign JD's national insurance number with format QQ 12 34 56 C.
+
+        Returns:
+            String: Randomly generated letters and numbers in correct format.
+        """
 
         # random ascii char
         _rac = lambda: random.choice(string.ascii_uppercase)
@@ -159,10 +234,17 @@ class JohnDoe:
         return f"{_rac()}{_rac()} {_random_int()} {_random_int()} {_random_int()} {_rac()}"
 
     @staticmethod
-    def address():
-        """Get random UK addresses information.
-        Postcode and area are genuine.
-        Street names are random choice from top 50"""
+    def address() -> dict:
+        """Assign JD's address.
+
+        Returns:
+            Dictionary:
+                House number:   Random integer 1 - 500.
+                Street:         Random choice of popular UK street names.
+                Area:           Random choice of any UK area.
+                Postcode:       Based on area.
+
+        """
 
         # Get random house number
         house_number = random.randint(1, 500)
@@ -188,11 +270,23 @@ class JohnDoe:
                 }
 
     @staticmethod
-    def banking_info():
-        """Get genuine UK bank card information provider.
-        Generate random 10 numbers to complete card."""
+    def banking_info() -> dict:
+        """Generate JD's banking information.
+
+        Returns:
+            Dictionary:
+                Bank:           Based on sortcode
+                Sortcode:       Random from list of UK banks
+                Account Number: Randomly generated
+                Card number:    Randomly generated
+                Provider:       Based on card number
+                Expiry date:    Randomly generated
+                CVV:            Randomly Generated
+        """
 
         def account_details() -> dict:
+            """Gets the accurate information from file"""
+
             with open("src/bank/sortcodes.txt", "r") as f:
                 lines = f.readlines()
                 l = random.choice(lines)
@@ -237,23 +331,12 @@ class JohnDoe:
                 "cvv": cvv
                 }
 
-    def birthday(self):
-        """Random birthday"""
+    def driving_license(self) -> str:
+        """Generates JD's driving license number in accurate format.
 
-        year = datetime.datetime.now().year - self.age
-        month = random.randint(1, datetime.datetime.now().month)
-        day = random.randint(1, 28)
-
-        # Always print double-digit birthdays
-        if day < 10:
-            day = "0" + str(day)
-        if month < 10:
-            month = "0" + str(month)
-
-        return f"{day}/{month}/{year}"
-
-    def driving_license(self):
-        """Driving license according to UK format"""
+        Returns:
+            String: Uses JD's gender, name, date of birth, and randomly generated characters.
+        """
 
         # The first five characters of the surname
         # (padded with 9s if less than 5 characters)
@@ -266,21 +349,21 @@ class JohnDoe:
 
         # The decade digit from the year of birth
         # (e.g. for 1987 it would be 8)
-        b = self.birthday.split("/")[2][2]
+        b = str(self.date_of_birth).split("/")[2][2]
 
         # The month of birth (7th character incremented
         # by 5 if driver is female i.e. 51–62 instead of 01–12)
-        if self.gender == "male":
-            c = self.birthday.split("/")[1]
+        if self.gender == "m":
+            c = str(self.date_of_birth).split("/")[1]
         else:
-            c = int(self.birthday.split("/")[1]) + 5
+            c = int(str(self.date_of_birth).split("/")[1]) + 5
 
         # The date within the month of birth
-        d = self.birthday.split("/")[0]
+        d = str(self.date_of_birth).split("/")[0]
 
         # The year digit from the year of birth
         # (e.g. for 1987 it would be 7)
-        e = self.birthday.split("/")[2][3]
+        e = str(self.date_of_birth).split("/")[2][3]
 
         # The first two initials of the first names
         # (padded with a 9 if no middle name)
@@ -296,20 +379,25 @@ class JohnDoe:
         g = "9"  # todo make this dynamic (as above with db)
 
         # Two computer check digits
-        h = f"{random.choice(string.ascii_uppercase)}{random.choice(string.ascii_uppercase)}"
+        h = f"{random.choice(string.ascii_uppercase)}" \
+            f"{random.choice(string.ascii_uppercase)}"
 
         # Appended, two digits representing the licence issue, which increases
         # by 1 for each licence issued
-        i = f"{random.randint(1, 9)}0"
+        i = f"{random.randint(1, 9)}"
 
         return "{}{}{}{}{}{}{}{} {}".format(a.upper(), b, c, d, e, f, g, h, i)
 
-    def email(self):
-        """Random email based on name of John Doe"""
+    def email(self) -> str:
+        """Generates JD's email address.
+
+        Returns:
+            String: Uses JD's first and last name with a random email provider
+        """
 
         # Email providers with UK TLD
-        providers = ["yahoo.com", "gmail.com", "live.com",
-                     "hotmail.com", "icloud.com", "msn.com"
+        providers = ["yahoo.co.uk", "gmail.co.uk", "live.co.uk",
+                     "hotmail.co.uk", "icloud.co.uk", "msn.co.uk"
                      ]
 
         provider = random.choice(providers)
@@ -319,8 +407,14 @@ class JohnDoe:
         return email
 
     @staticmethod
-    def ip_address():
-        """Generate a random IP based on a list of genuine UK IP addresses blocks."""
+    def ip_address() -> str:
+        """Generate JD's IP address from UK IP blocks.
+
+        A random line is chosen and the minimum and maximum IP address block is used to generate the IP.
+
+        Returns:
+            String: Randomly generated IP based on provider IP block.
+        """
 
         # Get random line from IP csv file
         with open(f"src/addresses/ip_address.csv") as f:
@@ -352,16 +446,21 @@ class JohnDoe:
 
         return "{}.{}.{}.{}".format(ip1, ip2, ip3, ip4)
 
-    def document(self):
+    def document(self) -> None:
+        """Generate and show Jd's identity documents by calling the generate() function of the respective classes.
+
+        Returns:
+            None: Upon success the generated cards will automatically open.
+        """
         ident_card = nic.NationalIdentityCard(forename=self.name.split(" ")[0],
                                               surname=self.name.split(" ")[1],
-                                              date_of_birth=self.birthday,
+                                              date_of_birth=self.date_of_birth,
                                               gender=self.gender,
                                               place_of_birth=self.address["area"])
 
         dl = driving_license.DrivingLicense(forename=self.name.split(" ")[0],
                                             surname=self.name.split(" ")[1],
-                                            date_of_birth=self.birthday,
+                                            date_of_birth=self.date_of_birth,
                                             place_of_birth=self.address["area"],
                                             gender=self.gender,
                                             dvla_number=self.driving_license,
@@ -370,55 +469,34 @@ class JohnDoe:
 
         bank_card = bank.Bank(name=self.name, cc_number=self.banking["card_number"], expiry=self.banking["expiry_date"])
 
-        from src import nino
-        nino = nino.Nino(self.name, self.nino)
+        nin = nino.Nino(self.name, self.nino)
 
-        nino.generate_card()  # Generate national insurance card
+        nin.generate_card()  # Generate national insurance card
         dl.generate_card()  # Generate driving license
         ident_card.generate_card()  # generate national identity card
         bank_card.generate()
 
-    def create(self):
+    def print(self) -> None:
+        """Prints JD's information to the console
+
+        Returns:
+            None: Prints self.__dict__ as json via self.json()
+        """
         print(self.json())
 
 
 def main():
     parser = argparse.ArgumentParser(description="Generate PII for testing environments")
     parser.add_argument("--name", type=str, help="Name (first last)")
-    parser.add_argument("--age", type=int, help="Age (>18)")
+    parser.add_argument("--dob", type=str, help="Date of birth (DD/MM/YY)")
     parser.add_argument("--gender", help="Gender (m/f)")
-    parser.add_argument("--document", action="store_true", help="Generate identity documents")
+    parser.add_argument("--docs", action="store_true", help="Generate identity documents")
+    parser.add_argument("--print", action="store_true", help="Print output to console")
+
     arguments = parser.parse_args()
     args_dict = vars(arguments)
 
-    if arguments.document:
-        jd = JohnDoe(**args_dict)
-
-        dl = driving_license.DrivingLicense(forename=jd.name.split(" ")[0],
-                                            surname=jd.name.split(" ")[1],
-                                            date_of_birth=jd.birthday,
-                                            place_of_birth=jd.address["area"],
-                                            gender=jd.gender,
-                                            dvla_number=jd.driving_license,
-                                            address=f"{jd.address['house_number']} {jd.address['street']}\n"
-                                                    f"{jd.address['area']} {jd.address['postcode']}")
-
-        ident_card = nic.NationalIdentityCard(forename=jd.name.split(" ")[0],
-                                              surname=jd.name.split(" ")[1],
-                                              date_of_birth=jd.birthday,
-                                              gender=jd.gender,
-                                              place_of_birth=jd.address["area"])
-
-        bank_card = bank.Bank(name=jd.name, cc_number=jd.banking["card_number"], expiry=jd.banking["expiry_date"])
-        ni = nino.Nino(name=jd.name, nino=jd.nino)
-
-        # Generate all the cards
-        dl.generate_card()
-        ident_card.generate_card()
-        bank_card.generate()
-        ni.generate_card()
-
-        print(jd.json())
+    JohnDoe(**args_dict)
 
 
 if __name__ == "__main__":
